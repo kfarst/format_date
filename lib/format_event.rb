03beba8c75130514374bc86a028d0731c1c90e00
Kevin Farst <kevin@letote.com>
2014-01-02 01:49:04 -0500

    fix class nesting

--- lib/format_event.rb
+++ lib/format_event.rb
@@ -1,30 +1,28 @@
 require "format_event/version"
 
 module FormatEvent
-  module FormatEvent
-    class Date < ::Liquid::Tag
-      def initialize(tag_name, date)
-        @date = date
-      end
-
-      def render(context)
-        date = @date.split(' ').first
-        date.gsub('-', '') if date
-      end
+  class Date < ::Liquid::Tag
+    def initialize(tag_name, date)
+      @date = date
     end
 
-    class Time < ::Liquid::Tag
-      def intialize(tag_name, datetime)
-        @datetime = datetime
-      end
-
-      def render(context)
-        time = @datetime.split(' ')[1]
-        time.gsub(':', '.') if time
-      end
+    def render(context)
+      date = @date.split(' ').first
+      date.gsub('-', '') if date
     end
   end
 
-  ::Liquid::Template.register_tag('format_event_date', FormatEvent::Date)
-  ::Liquid::Template.register_tag('format_event_time', FormatEvent::Time)
+  class Time < ::Liquid::Tag
+    def intialize(tag_name, datetime)
+      @datetime = datetime
+    end
+
+    def render(context)
+      time = @datetime.split(' ')[1]
+      time.gsub(':', '.') if time
+    end
+  end
 end
+
+::Liquid::Template.register_tag('format_event_date', FormatEvent::Date)
+::Liquid::Template.register_tag('format_event_time', FormatEvent::Time)
